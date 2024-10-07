ef749b612f91f381bb13b77f6c7502e9b77c3380
Snegov1k <Alekseevich.m.k@gmail.com>
2024-10-07 19:13:50 +0300

    add new_avatar_notification message

--- generate.sh
+++ generate.sh
@@ -5,3 +5,7 @@ protoc  --go_out=./  \
         avatar.proto
 
 protoc --doc_out=. --doc_opt=markdown,README.md ./avatar.proto
+
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        new_avatar_notification.proto
